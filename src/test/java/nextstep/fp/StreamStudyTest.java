73c272364f913e483c326e630259b8fc4c270d09
paige.k <paige.k@kakaocorp.com>
2019-07-20 11:06:30 +0900

    오타 수정

--- src/test/java/nextstep/fp/StreamStudyTest.java
+++ src/test/java/nextstep/fp/StreamStudyTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class sStreamStudyTest {
+public class StreamStudyTest {
 	private List<Integer> numbers;
 	
 	@BeforeEach
