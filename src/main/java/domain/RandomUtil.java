b8f7fb55fea0a1ea149a8809eb030beade204f00
willing1026 <34908960+willing1026@users.noreply.github.com>
2019-07-20 11:53:57 +0900

    fix : GeneratorPoint제거, 연관소스 수정

--- src/main/java/domain/RandomUtil.java
+++ src/main/java/domain/RandomUtil.java
@@ -2,18 +2,12 @@ package domain;
 
 import java.util.Random;
 
-public class RandomUtil implements GeneratorPoint {
-
+public class RandomUtil {
     public static boolean getBoolean(boolean beforeRight) {
         if(beforeRight) {
             return Boolean.FALSE;
         }
 
         return new Random().nextBoolean();
-    }
-
-    @Override
-    public boolean generate() {
-        return false;
     }
 }
