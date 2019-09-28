2ab0d52c6b60e84f6e11e9b27a09b214c3d61973
RudiaMoon <impactyoung@gmail.com>
2019-09-28 20:34:19 +0900

    Update 레벨에 따른 확률 작업중

--- src/main/java/domain/RandomUtil.java
+++ src/main/java/domain/RandomUtil.java
@@ -3,16 +3,36 @@ package domain;
 import java.util.Random;
 
 public class RandomUtil {
-
     public static boolean getBoolean() {
         return new Random().nextBoolean();
     }
 
     public static boolean getBoolean(boolean beforeRight) {
-        if(beforeRight) {
+        if (beforeRight) {
             return Boolean.FALSE;
         }
 
-        return new Random().nextBoolean();
+        return test();
+    }
+
+    public static int random() {
+        return new Random().nextInt(10) + 1;
+    }
+
+    public static boolean test(){
+        //1~9 하:1~3 중: 1~6, 상:1~9
+        if("하".equals("하")&& random() < 4){
+            return true;
+        }
+
+        if("중".equals() && random() < 7){
+            return true;
+        }
+
+        if("상".equals() && random() < 9){
+            return true;
+        }
+
+        return false;
     }
 }
