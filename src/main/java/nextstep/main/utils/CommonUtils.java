301eb838b5fac25174eac5a1fe4f4e3950fa00b7
RudiaMoon <impactyoung@gmail.com>
2019-09-28 20:03:55 +0900

    Udpate pushtest

--- src/main/java/nextstep/main/utils/CommonUtils.java
+++ src/main/java/nextstep/main/utils/CommonUtils.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class CommonUtils {
+public class CommonUtils {//push test
     public static String[] split(String value) {
         return value.split(Consts.NAME_SEPARATOR);
     }
